7fdb783d97c312529125d35a39337fd244892b59
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-09 13:40:07 -0400

    made an ifstatement

--- fruitsales.js
+++ fruitsales.js
@@ -5,4 +5,6 @@ let fruitPrices = [1, 1.5, 7, 12]
 for (i = 0 ; i < fruitInventory.length; i++){
     console.log(` ${fruitInventory[i]}`)
 }
-
+ if (fruitInventory[i] != fruitInventory.length){
+     console.log(fruitInventory)
+ }
