ea2e8fcd2d3cddf71b799684b17e412087dfe508
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-11 11:20:31 -0400

    updated fruit stand

--- fruitsales.js
+++ fruitsales.js
@@ -2,17 +2,9 @@ let fruitInventory = ["Apples", "Peaces", "Pumpkins", "Endives" ];
 let fruitPrices = [1, 1.5, 7, 12]
 
 
-<<<<<<< HEAD
 for (i = 0 ; i < fruitInventory.length; i++){
     console.log(` ${fruitInventory[i]}`)
 }
  if (fruitInventory[i] != fruitInventory.length){
      console.log(fruitInventory)
  }
-=======
-for (i = 0 ; i < fruitInventory[i]; i++);
-   
- if (i < fruitInventory.length){
-    console.log(` ${fruitInventory}`)
- }
->>>>>>> 81af9fe1a7b3dfd7329fb269e44681c42cc81c02
